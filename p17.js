5f9f146c788ac2bca483f2d7bef22d5cb6e4d231
nuppgae <88790329+mommomoaz@users.noreply.github.com>
2022-07-29 23:32:38 +0900

    Add files via upload

---
+++ p17.js
@@ -0,0 +1,15 @@
+let yuju = prompt("키를 입력하세요");
+if (yuju>=150) {
+console.log(yes);}
+else{
+    console.log(no);
+}
+
+
+// const height = prompt("키를 입력하세요.");
+
+// if (height >= 150){
+//   console.log("YES");
+// } else {
+//   console.log("NO");
+// }
