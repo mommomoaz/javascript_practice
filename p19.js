13d81c0cb70a0fdd043a1b807b79fefe37445a47
nuppgae <88790329+mommomoaz@users.noreply.github.com>
2022-08-01 22:08:04 +0900

    Add files via upload

---
+++ p19.js
@@ -0,0 +1,14 @@
+let squaredNumber = Math.pow(a,b);
+console.log("5*5 = ",squaredNumber);
+
+
+// const n = prompt('수를 입력하세요.').split(' ');
+// //스플릿:문자열을 구분하는데 쓰임
+
+// console.log(Math.pow(parseInt(n[0], 10), parseInt(n[1], 10)));
+// //parseInt() - 문자열을 정수로 바꾸는 함수입니다.
+// parseInt( string, n )
+// string을 n진법일 때의 값으로 바꿉니다. n은 옵션으로 2부터 36까지 입력할 수 있습니다. 입력하지 않으면 10으로 처리합니다.
+// string의 처리는 parseFloat()와 거의 같습니다.
+// 소수 부분은 버립니다.
+// 0x로 시작하면 16진법으로 처리합니다.
