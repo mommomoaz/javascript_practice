acc0b37b7fc54840f31c1be489234e16b510ce0b
nuppgae <88790329+mommomoaz@users.noreply.github.com>
2022-08-01 22:31:04 +0900

    Add files via upload

---
+++ p20.js
@@ -0,0 +1,14 @@
+// const score = prompt('값을 입력하세요').split(' ');
+// console.log(
+//     (parseInt(n[0], 10))%(parseInt(n[1], 10))
+// );
+
+
+// const n = prompt('수를 입력하세요.').split(' ');
+
+// const result = Math.floor(parseInt(n[0], 10) / parseInt(n[1], 10));
+// const left = parseInt(n[0], 10) % parseInt(n[1], 10);
+
+// console.log(result, left);
+
+// Math.floor:내림
